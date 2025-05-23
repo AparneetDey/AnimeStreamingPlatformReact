fc141e03837091c13d60f37b6ba2911f4b42130f
Your Name <aparnitstudy@gmail.com>
2025-05-23 12:56:11 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
       <div className='pattern' />
+      <img src="/BG.png" alt="banner" className='absolute'/>
 
       <div className='wrapper'>
         <header>
+          <img src="/hero.png" alt="Hero Banner" className='text-white'/>
           <h1>Find The <span className='text-gradient'>Movies</span> That You Love Without Any Hassle</h1>
         </header>
 
-        <p className='text-white cursor-pointer'>Search</p>
+        <Search searchTerm = {searchTerm} setSearchTerm = {setSearchTerm} />
       </div>
     </main>
   )
