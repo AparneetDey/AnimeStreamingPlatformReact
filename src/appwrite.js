fe82efe7fe752eb27c89b314db829ad408075873
Your Name <aparnitstudy@gmail.com>
2025-06-28 19:38:33 +0530

    vercel set up again 1

--- src/appwrite.js
+++ src/appwrite.js
@@ -3,10 +3,9 @@ import { Client, Databases, Query, ID } from 'appwrite';
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
-const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
 
 const client = new Client()
-    .setEndpoint(ENDPOINT)
+    .setEndpoint('https://nyc.cloud.appwrite.io/v1')
     .setProject(PROJECT_ID)
 
 const database = new Databases(client)
